f818dfe462bd689a16c504537e144a40a06abe90
Clodagh Walsh <clodaghwalsh17@gmail.com>
2022-03-26 21:24:46 +0000

    WIP: Phone register
    Signed-off-by: Clodagh Walsh <clodaghwalsh17@gmail.com>

--- app/src/main/java/com/example/cs4084/LoginActivity.java
+++ app/src/main/java/com/example/cs4084/LoginActivity.java
@@ -2,7 +2,11 @@ package com.example.cs4084;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +14,39 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        SharedPreferences preferences = getSharedPreferences("login",MODE_PRIVATE);
+        Button login = (Button) findViewById(R.id.loginButton);
+        Button register = (Button) findViewById(R.id.toRegisterActivity);
+
+        if(preferences.getBoolean("isLoggedIn",false)) {
+            openMainActivity();
+        }
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                // if(accountExists) and password right
+                openMainActivity();
+                preferences.edit().putBoolean("isLoggedIn",true).apply();
+                //else error message
+            }
+        });
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openRegisterActivity();
+            }
+        });
+    }
+
+    public void openMainActivity() {
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void openRegisterActivity() {
+        Intent intent = new Intent(this,RegisterActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/cs4084/RegisterActivity.java
+++ app/src/main/java/com/example/cs4084/RegisterActivity.java
@@ -7,23 +7,38 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class RegisterActivity extends AppCompatActivity {
+    private EditText name;
+    private EditText number;
+    private EditText password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-        SharedPreferences preferences = getSharedPreferences("login",MODE_PRIVATE);
-        Button register = (Button) findViewById(R.id.registerButton);
 
-        if(preferences.getBoolean("isLoggedIn",false)) {
-            openMainActivity();
-        }
+        Button register = (Button) findViewById(R.id.registerButton);
+        name = findViewById(R.id.editTextName);
+        number = findViewById(R.id.phoneNumber);
+        password = findViewById(R.id.editTextPassword);
+
+        SharedPreferences preferences = getSharedPreferences("login",MODE_PRIVATE);
+
         register.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v){
-                openMainActivity();
-                preferences.edit().putBoolean("isLoggedIn",true).apply();
+            @Override
+            public void onClick(View view){
+                // check ac doesnt already exist && !newAccount
+                 if(validateName() && validateNumber() && validatePassword()) {
+                    Intent intent = new Intent(RegisterActivity.this,VerifyActivity.class);
+                    intent.putExtra("number",number.getText().toString());
+                    startActivity(intent);
+                    //provide emergency contacts this is a fragment also send these to db
+                    //register();
+                    //openMainActivity(); //??
+                    //preferences.edit().putBoolean("isLoggedIn",true).apply();
+                }
             }
         });
     }
@@ -32,4 +47,47 @@ public class RegisterActivity extends AppCompatActivity {
         Intent intent = new Intent(this,MainActivity.class);
         startActivity(intent);
     }
+
+    public void register() {
+//        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    }
+
+    private boolean validateName() {
+        if(name.getText().toString().isEmpty()) {
+            name.setError("Please provide your name");
+            return false;
+        }
+        return true;
+    }
+
+    private boolean validateNumber() {
+        String phoneNumber = number.getText().toString();
+        if(phoneNumber.isEmpty()) {
+            number.setError("Please provide your phone number");
+            return false;
+        }
+        else if(phoneNumber.length() != 10 || !phoneNumber.substring(0,2).equals("08")) {
+            number.setError("Please enter a valid phone number");
+            return false;
+        }
+        return true;
+    }
+
+    private boolean validatePassword() {
+        String pwd = password.getText().toString();
+        if(pwd.isEmpty()) {
+            password.setError("Please provide a password");
+            return false;
+        }
+        else if(pwd.length() < 8) {
+            password.setError("Password must be a least 8 characters");
+            return false;
+        }
+        return true;
+    }
+
+    /*private boolean newAccount() {
+        return true;
+    }*/
 }
