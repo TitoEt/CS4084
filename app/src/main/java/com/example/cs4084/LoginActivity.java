62bfe8d87c25b61178aabd0f9eee105d4898f70d
Clodagh Walsh <clodaghwalsh17@gmail.com>
2022-03-30 13:09:33 +0100

    Register and Login implemented
    Signed-off-by: Clodagh Walsh <clodaghwalsh17@gmail.com>

--- app/src/main/java/com/example/cs4084/LoginActivity.java
+++ app/src/main/java/com/example/cs4084/LoginActivity.java
@@ -1,34 +1,47 @@
 package com.example.cs4084;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
-
+    private FirebaseAuth auth;
+    private EditText email;
+    private EditText password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        auth = FirebaseAuth.getInstance();
+        if(auth.getCurrentUser() != null) {
+            openMainActivity();
+            finish();
+        }
+
         setContentView(R.layout.activity_login);
-        SharedPreferences preferences = getSharedPreferences("login",MODE_PRIVATE);
+
         Button login = (Button) findViewById(R.id.loginButton);
         Button register = (Button) findViewById(R.id.toRegisterActivity);
-
-        if(preferences.getBoolean("isLoggedIn",false)) {
-            openMainActivity();
-        }
+        email = (EditText) findViewById(R.id.email);
+        password = (EditText) findViewById(R.id.password);
 
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view){
-                // if(accountExists) and password right
-                openMainActivity();
-                preferences.edit().putBoolean("isLoggedIn",true).apply();
-                //else error message
+                if(isEmailFilled() && isPasswordFilled()) {
+                    login();
+                }
             }
         });
 
@@ -40,13 +53,46 @@ public class LoginActivity extends AppCompatActivity {
         });
     }
 
-    public void openMainActivity() {
+    private void openMainActivity() {
         Intent intent = new Intent(this,MainActivity.class);
         startActivity(intent);
     }
 
-    public void openRegisterActivity() {
+    private void openRegisterActivity() {
         Intent intent = new Intent(this,RegisterActivity.class);
         startActivity(intent);
+    }
+
+    private void login() {
+        auth.signInWithEmailAndPassword(email.getText().toString(),password.getText().toString())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()) {
+                            Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+                            startActivity(intent);
+                            finish();
+                        }
+                        else {
+                            Toast.makeText(LoginActivity.this,"Email or password are incorrect", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+    }
+
+    private boolean isEmailFilled() {
+        if(email.getText().toString().isEmpty()) {
+            email.setError("Please enter your email");
+            return false;
+        }
+        return true;
+    }
+
+    private boolean isPasswordFilled() {
+        if(password.getText().toString().isEmpty()) {
+            password.setError("Please enter your password");
+            return false;
+        }
+        return true;
     }
 }
--- app/src/main/java/com/example/cs4084/RegisterActivity.java
+++ app/src/main/java/com/example/cs4084/RegisterActivity.java
@@ -1,74 +1,72 @@
 package com.example.cs4084;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterActivity extends AppCompatActivity {
     private EditText name;
-    private EditText number;
+    private EditText email;
     private EditText password;
+    private EditText number;
+    private FirebaseAuth auth;
+    //send name and phone to db
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        Button register = (Button) findViewById(R.id.registerButton);
-        name = findViewById(R.id.editTextName);
-        number = findViewById(R.id.phoneNumber);
-        password = findViewById(R.id.editTextPassword);
+        auth = FirebaseAuth.getInstance();
 
-        SharedPreferences preferences = getSharedPreferences("login",MODE_PRIVATE);
+        Button register = (Button) findViewById(R.id.registerButton);
+        name = (EditText) findViewById(R.id.editTextName);
+        email = (EditText) findViewById(R.id.editTextEmail);
+        password = (EditText) findViewById(R.id.editTextPassword);
+        number =  (EditText) findViewById(R.id.editTextNumber);
 
         register.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view){
-                // check ac doesnt already exist && !newAccount
-                 if(validateName() && validateNumber() && validatePassword()) {
-                    Intent intent = new Intent(RegisterActivity.this,VerifyActivity.class);
-                    intent.putExtra("number",number.getText().toString());
-                    startActivity(intent);
-                    //provide emergency contacts this is a fragment also send these to db
-                    //register();
-                    //openMainActivity(); //??
-                    //preferences.edit().putBoolean("isLoggedIn",true).apply();
+            public void onClick(View view) {
+                if(validateName() && validateEmail() && validateNumber() && validatePassword()) {
+                    register();
                 }
             }
         });
     }
 
-    public void openMainActivity() {
-        Intent intent = new Intent(this,MainActivity.class);
-        startActivity(intent);
-    }
-
-    public void register() {
-//        FirebaseFirestore db = FirebaseFirestore.getInstance();
-
+    private void register() {
+        auth.createUserWithEmailAndPassword(email.getText().toString(),password.getText().toString())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()) {
+                            Intent intent = new Intent(RegisterActivity.this,MainActivity.class);
+                            startActivity(intent);
+                            finish();
+                        }
+                        else {
+                            Toast.makeText(RegisterActivity.this, "An account with this email already exists", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
     }
 
     private boolean validateName() {
         if(name.getText().toString().isEmpty()) {
             name.setError("Please provide your name");
-            return false;
-        }
-        return true;
-    }
-
-    private boolean validateNumber() {
-        String phoneNumber = number.getText().toString();
-        if(phoneNumber.isEmpty()) {
-            number.setError("Please provide your phone number");
-            return false;
-        }
-        else if(phoneNumber.length() != 10 || !phoneNumber.substring(0,2).equals("08")) {
-            number.setError("Please enter a valid phone number");
             return false;
         }
         return true;
@@ -87,7 +85,32 @@ public class RegisterActivity extends AppCompatActivity {
         return true;
     }
 
-    /*private boolean newAccount() {
+    private boolean validateEmail() {
+        String userEmail = email.getText().toString();
+
+        if(userEmail.isEmpty()) {
+            email.setError("Please provide your email");
+            return false;
+        }
+        else if(!Patterns.EMAIL_ADDRESS.matcher(userEmail).matches()) {
+            email.setError("Please enter a valid email address");
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+
+    private boolean validateNumber() {
+        String phoneNumber = number.getText().toString();
+        if(phoneNumber.isEmpty()) {
+            number.setError("Please provide your phone number");
+            return false;
+        }
+        else if(phoneNumber.length() != 10 || !phoneNumber.substring(0,2).equals("08")) {
+            number.setError("Please enter a valid phone number");
+            return false;
+        }
         return true;
-    }*/
+    }
 }
