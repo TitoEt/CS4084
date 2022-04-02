48d54fa805e1ad87efa2d362aeb4eb93e2818b82
TitoEt <19248547@studentmail.ul.ie>
2022-04-02 20:26:20 +0100

    Helpline page, with working links and descriptions

--- app/src/main/java/com/example/cs4084/HelplineActivity.java
+++ app/src/main/java/com/example/cs4084/HelplineActivity.java
@@ -14,5 +14,14 @@ public class HelplineActivity extends AppCompatActivity {
         setContentView(R.layout.activity_helpline);
         TextView text = (TextView) findViewById(R.id.textView);
         text.setMovementMethod(LinkMovementMethod.getInstance());
+
+        TextView text2 = (TextView) findViewById(R.id.textView2);
+        text2.setMovementMethod(LinkMovementMethod.getInstance());
+
+        TextView text3 = (TextView) findViewById(R.id.textView3);
+        text3.setMovementMethod(LinkMovementMethod.getInstance());
+
+        TextView text4 = (TextView) findViewById(R.id.textView4);
+        text4.setMovementMethod(LinkMovementMethod.getInstance());
     }
 }
