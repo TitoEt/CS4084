e063bfe8be0e93d56d9b2fc72ef9aaf26accfb85
TitoEt <19248547@studentmail.ul.ie>
2022-04-02 20:18:00 +0100

    Helpline page, with working links and descriptions

--- app/src/main/java/com/example/cs4084/HelplineActivity.java
+++ app/src/main/java/com/example/cs4084/HelplineActivity.java
@@ -2,7 +2,11 @@ package com.example.cs4084;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.widget.Scroller;
+import android.widget.TextView;
 
 public class HelplineActivity extends AppCompatActivity {
 
@@ -10,5 +14,18 @@ public class HelplineActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_helpline);
+        TextView text = (TextView) findViewById(R.id.textView);
+        text.setMovementMethod(LinkMovementMethod.getInstance());
+
+        TextView text1 = (TextView) findViewById(R.id.textView2);
+        text1.setMovementMethod(LinkMovementMethod.getInstance());
+
+        TextView text2 = (TextView) findViewById(R.id.textView3);
+        text2.setMovementMethod(LinkMovementMethod.getInstance());
+
+        TextView text3 = (TextView) findViewById(R.id.textView4);
+        text3.setMovementMethod(LinkMovementMethod.getInstance());
+
+
     }
 }
