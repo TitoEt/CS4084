377caa64ac38bc46852af81faf56a75d1d46154e
Clodagh Walsh <clodaghwalsh17@gmail.com>
2022-04-08 21:28:35 +0100

    Update activity_plan_trip.xml
    Signed-off-by: Clodagh Walsh <clodaghwalsh17@gmail.com>

---
+++ app/src/main/java/com/example/cs4084/TimePickerFragment.java
@@ -0,0 +1,13 @@
+package com.example.cs4084;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TimePickerFragment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
