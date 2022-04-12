293473f65059f2e6ca618f128e746720647b9c2d
TTLawlor <lawlortamara@gmail.com>
2022-04-12 21:42:17 +0100

    WIP: Update dashboard

--- app/src/main/java/com/example/cs4084/MainActivity.java
+++ app/src/main/java/com/example/cs4084/MainActivity.java
@@ -7,18 +7,25 @@ import androidx.fragment.app.DialogFragment;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
+import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.TimePickerDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
 import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.text.Html;
+import android.telephony.SmsManager;
 import android.text.format.DateFormat;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.TimePicker;
 
@@ -26,22 +33,95 @@ import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = "YOUR-TAG-NAME";
+    private static final int PERMISSION_REQUEST_CODE = 1;
     TextView text1;
     Button bt;
+    private String emergencyContact;
+    private String name;
+    private String messageToSend;
+    private MediaPlayer mp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        mp = MediaPlayer.create(this, R.raw.alarm);
 
-        text1 = findViewById(R.id.text_1);
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.alarms_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                if(mp != null && mp.isPlaying() == true) {
+                    mp.reset();
+                }
+                switch (i) {
+                    case 0:
+                        mp = null;
+                        break;
+                    case 1:
+                        mp = MediaPlayer.create(MainActivity.this, R.raw.car_alarm);
+                        break;
+                    case 2:
+                        mp = MediaPlayer.create(MainActivity.this, R.raw.alarm);
+                        break;
+                    case 3:
+                        mp = MediaPlayer.create(MainActivity.this, R.raw.fire_alarm);
+                        break;
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                mp = null;
+            }
+        });
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.SEND_SMS) == PackageManager.PERMISSION_DENIED) {
+                Log.d("permission", "permission denied to SEND_SMS - requesting it");
+                String[] permissions = {Manifest.permission.SEND_SMS};
+                requestPermissions(permissions, PERMISSION_REQUEST_CODE);
+            }
+        }
+
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        Intent intent = getIntent();
+        DocumentReference docRef = db.collection("users").document(intent.getStringExtra("uid"));
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        Log.d(TAG, "DocumentSnapshot data: " + document.getData());
+                        emergencyContact = document.getString("emergencyContact");
+                        name = document.getString("name");
+                    } else {
+                        Log.d(TAG, "No such document");
+                    }
+                } else {
+                    Log.d(TAG, "get failed with ", task.getException());
+                }
+            }
+        });
 //        bt = findViewById(R.id.panic);
 //        bt.setOnClickListener(new View.OnClickListener() {
 //            @Override
@@ -54,12 +134,27 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    public void endAlarm(View view) {
+        new AlertDialog.Builder(this)
+                .setMessage("Are you sure you want to stop the alarm?")
+                .setCancelable(false)
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        if(mp != null) {
+                            mp.pause();
+                        }
+                    }
+                })
+                .setNegativeButton("No", null)
+                .show();
+    }
+
     public void sendMessage(View view) {
-        final MediaPlayer mp = MediaPlayer.create(this, R.raw.alarm);
-//        mp.stop();
         if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
-            mp.setLooping(true);
-            mp.start();
+            if(mp != null && mp.isPlaying() == false) {
+                mp.setLooping(true);
+                mp.start();
+            }
             getLocation();
         }else {
             ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 44);
@@ -102,10 +197,16 @@ public class MainActivity extends AppCompatActivity {
                     try {
                         Geocoder geocoder = new Geocoder(MainActivity.this, Locale.getDefault());
                         List<Address> addresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
-                        text1.setText(Html.fromHtml("<font ><b>Latitude :</b></font>" + addresses.get(0).getLatitude() +"<font ><b><br>Longitude :</b></font>" + addresses.get(0).getLongitude()
-                                +"<font ><b><br>Country :</b></font>" + addresses.get(0).getCountryName()
-                                +"<font ><b><br>Locality :</b></font>" + addresses.get(0).getLocality()
-                                +"<font ><b><br>address :</b></font>" + addresses.get(0).getAddressLine(0)));
+                        messageToSend = "!!!EMERGENCY ALERT!!!\n\n" + name
+                                + " has alerted that they are in danger.\n\nTheir current location is:"
+                                + "\nLocality : " + addresses.get(0).getLocality()
+                                +"\naddress : " + addresses.get(0).getAddressLine(0)
+                                + "\nLatitude : " + addresses.get(0).getLatitude()
+                                +"\nLongitude : " + addresses.get(0).getLongitude();
+                        SmsManager sms = SmsManager.getDefault();
+                        ArrayList<String> message = sms.divideMessage(messageToSend);
+                        sms.sendMultipartTextMessage(emergencyContact, null, message, null, null);
+
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
--- app/src/main/java/com/example/cs4084/SelectEmergencyContactActivity.java
+++ app/src/main/java/com/example/cs4084/SelectEmergencyContactActivity.java
@@ -1,24 +1,30 @@
 package com.example.cs4084;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.ContactsContract;
+import android.telephony.SmsManager;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 public class SelectEmergencyContactActivity extends AppCompatActivity {
     private static final int CONTACT_REQUEST_CODE = 100;
+    private static final int PERMISSION_REQUEST_CODE = 1;
     private String emergencyContact;
 
     private Button select;
@@ -39,6 +45,13 @@ public class SelectEmergencyContactActivity extends AppCompatActivity {
     }
 
     private void selectEmergencyContact() {
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.SEND_SMS) == PackageManager.PERMISSION_DENIED) {
+                Log.d("permission", "permission denied to SEND_SMS - requesting it");
+                String[] permissions = {Manifest.permission.SEND_SMS};
+                requestPermissions(permissions, PERMISSION_REQUEST_CODE);
+            }
+        }
         Intent intent = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
         intent.setType(ContactsContract.CommonDataKinds.Phone.CONTENT_TYPE);
         startActivityForResult(intent,CONTACT_REQUEST_CODE);
@@ -63,6 +76,8 @@ public class SelectEmergencyContactActivity extends AppCompatActivity {
                     cursor.close();
                     storeUserInfo();
                     Intent intent = new Intent(this,MainActivity.class);
+                    intent.putExtra("emergencyContact", emergencyContact);
+                    intent.putExtra("uid", intent.getStringExtra("uid"));
                     startActivity(intent);
                     finish();
                 }
@@ -87,5 +102,18 @@ public class SelectEmergencyContactActivity extends AppCompatActivity {
         userData.put("emergencyContact",emergencyContact);
 
         db.collection("users").document(intent.getStringExtra("uid")).set(userData);
+        
+        sendMessage();
+    }
+
+    public void sendMessage() {
+        String messageToSend;
+        Intent intent = getIntent();
+        
+        String formattedName = intent.getStringExtra("name");
+        messageToSend = "SECURUS UPDATE\n\n" + formattedName + " has chosen you as their emergency contact.\nYou will be notified of their current location if they indicate that they are in danger.\n";
+        SmsManager sms = SmsManager.getDefault();
+        ArrayList<String> message = sms.divideMessage(messageToSend);
+        sms.sendMultipartTextMessage(emergencyContact, null, message, null, null);
     }
 }
