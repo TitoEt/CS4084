7043253a6be45ac82216bf92f58714b279e102d4
Clodagh Walsh <clodaghwalsh17@gmail.com>
2022-04-13 17:17:23 +0100

    Add broadcast receiver
    Signed-off-by: Clodagh Walsh <clodaghwalsh17@gmail.com>

--- app/src/main/java/com/example/cs4084/TimePickerFragment.java
+++ app/src/main/java/com/example/cs4084/TimePickerFragment.java
@@ -1,17 +1,25 @@
 package com.example.cs4084;
 
+import android.app.AlarmManager;
 import android.app.Dialog;
+import android.app.PendingIntent;
 import android.app.TimePickerDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.text.format.DateFormat;
 import android.widget.TimePicker;
+import android.widget.Toast;
 
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.DialogFragment;
 
 import java.util.Calendar;
+import java.util.Date;
 
 public class TimePickerFragment extends DialogFragment implements TimePickerDialog.OnTimeSetListener {
-
+    private static final int BROADCAST_REQUEST_CODE = 200;
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
 
@@ -20,11 +28,27 @@ public class TimePickerFragment extends DialogFragment implements TimePickerDial
         int hour = c.get(Calendar.HOUR_OF_DAY);
         int minute = c.get(Calendar.MINUTE);
         TimePickerDialog timeDialog = new TimePickerDialog(getActivity(), this, hour, minute, DateFormat.is24HourFormat(getActivity()));
-        timeDialog.setTitle("Enter your estimated return time for this trip");
+        timeDialog.setTitle("Enter your estimated return time to start this trip.");
+
         return timeDialog;
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-        // start timer and check
+        Context context = getActivity();
+        Intent intent = new Intent(context, TimerReceiver.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(context,BROADCAST_REQUEST_CODE,intent,0);
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.HOUR_OF_DAY, view.getHour());
+        calendar.set(Calendar.MINUTE, view.getMinute());
+
+        Date departTime = new Date();
+        Date returnTime = calendar.getTime();
+        long duration = returnTime.getTime() - departTime.getTime();
+
+        AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        alarmManager.set(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + duration, pendingIntent);
+        Toast.makeText(context, "Expected Trip Duration is " + duration/60000 + " minutes", Toast.LENGTH_LONG).show();
     }
 }
---
+++ app/src/main/java/com/example/cs4084/TimerReceiver.java
@@ -0,0 +1,16 @@
+package com.example.cs4084;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Vibrator;
+import android.widget.Toast;
+
+public class TimerReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Vibrator vibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
+        vibrator.vibrate(5000);
+    }
+}
