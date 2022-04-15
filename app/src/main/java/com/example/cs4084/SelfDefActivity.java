6d11a6a883ab118a3ed886973704886f85575903
TitoEt <19248547@studentmail.ul.ie>
2022-04-15 13:45:26 +0100

    self def update, video upload, issues with layout.

--- app/src/main/java/com/example/cs4084/SelfDefActivity.java
+++ app/src/main/java/com/example/cs4084/SelfDefActivity.java
@@ -1,19 +1,39 @@
 package com.example.cs4084;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.MediaController;
 import android.widget.VideoView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SelfDefActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    List<String> defensemoves;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_selfdef);
-        
+        recyclerView = findViewById(R.id.recVi);
+        defensemoves = new ArrayList<>();
+        defensemoves.add("Disarm");
+        defensemoves.add("Go for vulnerable spots");
+        defensemoves.add("How to punch safely");
+        defensemoves.add("Escape bindings");
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(linearLayoutManager);
+        CustomAdapter customAdapter=new CustomAdapter((ArrayList<String>) defensemoves, SelfDefActivity.this);
+        recyclerView.setAdapter(customAdapter);
+
         VideoView videoView = findViewById(R.id.video_view);
         String videoPath = "android.resource://com.example.cs4084/" + getPackageName() + "/" + R.raw.selfdef;
         Uri uri = Uri.parse(videoPath);
@@ -23,7 +43,5 @@ public class SelfDefActivity extends AppCompatActivity {
         videoView.setMediaController(mediaController);
         mediaController.setAnchorView(videoView);
 
-
-        
     }
 }
